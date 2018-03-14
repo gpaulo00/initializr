b2ba583c9a6230e8693feef423b80fee2f984d3d
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2018-03-13 20:28:07 -0400

    add spec2 and mocks.cr

---
+++ spec/schema_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+Spec2.describe Initializr::Schema do
+  describe "Script" do
+    it "works" do
+      expect(true).to be(true)
+    end
+  end
+end
---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec2-mocks"
+require "../src/index"
